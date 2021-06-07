2450f2e7113167e278480bbae68b7ccc76b31aa1
astricus <astricus@hotmail.com>
2021-06-07 15:04:51 +0300

    added tests for signup, signin, signout and currentuser routes

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -19,7 +19,7 @@ it('returns a 400 with an invalid email', async () => {
       password: 'password',
     })
     .expect(400);
-})
+});
 
 it('returns a 400 with an invalid password', async () => {
   return request(app)
@@ -29,11 +29,38 @@ it('returns a 400 with an invalid password', async () => {
       password: 'p',
     })
     .expect(400);
-})
+});
 
 it('returns a 400 with missing email and password', async () => {
-  return request(app)
+  return request(app).post('/api/users/signup').send({}).expect(400);
+});
+
+it('disallows duplicate emails', async () => {
+  await request(app)
     .post('/api/users/signup')
-    .send({})
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
     .expect(400);
+});
+
+it('sets a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
 })
